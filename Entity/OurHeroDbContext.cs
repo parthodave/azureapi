88260d831ac6b5fc2eed5528302d7ddf1168150f
parthodave <parthodave@gmail.com>
2024-12-15 20:40:49 +0530

    initial commit

---
+++ Entity/OurHeroDbContext.cs
@@ -0,0 +1,27 @@
+﻿using DotNet8WebAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet8WebAPI.Entity
+{
+    public class OurHeroDbContext : DbContext
+    {
+        public OurHeroDbContext(DbContextOptions<OurHeroDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<OurHero> OurHeros { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<OurHero>().HasData(
+                new OurHero
+                {
+                    Id = 1,
+                    FirstName = "System",
+                    LastName = "",
+                    isActive=true,
+                }
+            );
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,100 @@
+using DotNet8WebAPI;
+using DotNet8WebAPI.Helpers;
+using DotNet8WebAPI.Model;
+using Azure.Identity;
+using DotNet8WebAPI.Entity;
+using DotNet8WebAPI.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+string connectionString = builder.Configuration.GetConnectionString("OurHeroConnectionString");
+
+builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
+
+builder.Services.AddSingleton<IOurHeroService, OurHeroService>();
+//builder.Services.AddSingleton<IOurHeroService, OurHeroService>();
+builder.Services.AddScoped<IUserService, UserService>();
+
+
+
+//builder.Services.AddTransient<IOurHeroService, OurHeroService>();
+builder.Services.AddScoped<IOurHeroService, OurHeroService>();
+
+builder.Services.AddSwaggerGen(swagger =>
+{
+    //This is to generate the Default UI of Swagger Documentation  
+    swagger.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "JWT Token Authentication API",
+        Description = ".NET 8 Web API"
+    });
+    // To Enable authorization using Swagger (JWT)  
+    swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+    });
+    swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                            new string[] {}
+
+                    }
+                });
+});
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+// Configure Key Vault
+var keyVaultUri = builder.Configuration["KeyVault:VaultUri"];
+builder.Configuration.AddAzureKeyVault(new Uri(keyVaultUri), new DefaultAzureCredential());
+
+
+
+var sqlConnectionString = builder.Configuration["ConnectionString"];
+
+builder.Services.AddDbContext<DotNet8WebAPI.OurHeroDbContext>(db => db.UseSqlServer(sqlConnectionString), ServiceLifetime.Singleton);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.UseMiddleware<JwtMiddleware>();
+app.MapControllers();
+
+app.Run();
